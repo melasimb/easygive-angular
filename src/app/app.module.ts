0d0c5f570c6574d77a6ec2f6b4ebecdb191b18c4
Melany <melasimb@gmail.com>
2020-06-15 21:20:38 +0200

    Customize theme #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponents} from './app-components';
+import {AppMaterialModule} from './app-material.module';
+import {AppServices} from './app-services';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponents.COMPONENTS,
+    AppComponents.DIALOGS
+  ],
+  entryComponents: [
+    AppComponents.DIALOGS
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
-  providers: [],
+  providers: [
+    AppServices.SERVICES
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
